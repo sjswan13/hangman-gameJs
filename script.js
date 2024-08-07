fa2ada78bc46ffac80a4c38e21cf34a775a1e3af
sjswan13 <sjswan13@gmail.com>
2024-08-07 12:42:51 -0600

    final stage of game functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('figure-part');
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ['application', 'programming', 'interface', 'wizard', 'special', 'reynauds', 'rhabdomylysis'];
 
@@ -36,7 +36,28 @@ function displayWord() {
 
 //Update Wrong Letters
 function updateWrongLettersEl() {
-  console.log('Update Wrong');
+  //Display Wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</Span>`)}
+  `;
+
+  //Display Parts
+   figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  //Check if Lost
+  if(wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost.';
+    popup.style.display = 'flex';
+  }
 }
 
 //show notification
@@ -71,6 +92,21 @@ window.addEventListener('keydown', e => {
       }
     }
   }
+});
+
+//Restart Game and Play Again
+playAgainBtn.addEventListener('click', () => {
+  //empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
 })
 
 displayWord();
+updateWrongLettersEl();
