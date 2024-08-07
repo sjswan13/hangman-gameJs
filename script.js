e7ea7f6d1070b4afe4bb6f939399dd2ab995649d
sjswan13 <sjswan13@gmail.com>
2024-08-07 12:19:19 -0600

    update wrong word functionality

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('figure-part');
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['application', 'programming', 'interface', 'wizard', 'special', 'reynauds', 'rhabdomylysis'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -33,5 +33,44 @@ function displayWord() {
     popup.style.display = 'flex';
   }
 }
+
+//Update Wrong Letters
+function updateWrongLettersEl() {
+  console.log('Update Wrong');
+}
+
+//show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show')
+}, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e => {
+  if(e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if(selectedWord.includes(letter)) {
+      if(!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if(!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+})
 
 displayWord();
